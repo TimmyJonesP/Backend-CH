47d45e4c81d20773942f4812183db94377620fe9
Sandro <sandrocarioli43@gmail.com>
2023-02-20 16:36:38 -0300

    no puedo hacer que me devuelva ya por id o por memory

--- desafio1.js
+++ desafio1.js
@@ -3,9 +3,9 @@ class ProductManager {
     description = ""
     price 
     thumbnail = ""
-    code = ""
+    code 
     stock 
-
+    
     constructor(title, description, price, thumbnail, code, stock){
         this.title = title
         this.description = description
@@ -18,16 +18,25 @@ class ProductManager {
     addProduct(product) {
 
         let newId
-        products.length == 0 ? newId = 1 : (newId = products[products.length - 1].id + 1)
+        this.products.length == 0 ? newId = 1 : (newId = products[products.length - 1].id + 1)
         newId = this.code
+        let newProduct = {...product, newId}
+        this.products.push(newProduct)
     }
 
     getProducts(){
         return this.products
     }
 
-    getProductById(id){
-        let seeker = this.products.find(p => p.id == id) || "Not found, create one."
+    getProductById(code){
+        let seeker = this.products.find(p => p.code == code) || "Not found, create one."
         return seeker
     }
 }
+let cucurucho = {title: "cucurucho", description: "Cucurucho de heladería", price: 199, thumbnail: "CucuruchoRico.jpeg", stock: 999}
+
+let TiendaHelado = new ProductManager
+
+TiendaHelado.addProduct(cucurucho)
+TiendaHelado.getProductById(1)
+TiendaHelado.getProducts
