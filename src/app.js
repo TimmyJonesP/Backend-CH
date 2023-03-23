82dc5d1dd57ccff40b2dc5f36fe376d16a0057f5
Sandro <sandrocarioli43@gmail.com>
2023-03-22 21:53:31 -0300

    wip

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const productRouter = require("../router/productsRouter")
 const cartRouter = require("../router/cartRouter")
+const handlebars = require("express-handlebars")
+const { Server } = require("socket.io")
 
 const app = express()
 const port = 3000
@@ -8,7 +10,16 @@ app.use(express.json())
 app.use("/api/productos", productRouter)
 app.use('/api/cart', cartRouter)
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname+"/public"))
+app.engine("handlebars", handlebars.engine())
+app.set("views", __dirname+"/view")
 
-app.listen(port, () =>{
+const io = new Server(httpServer)
+
+const httpServer = app.listen(port, () =>{
     console.log(`Server running at port ${port}`)
+})
+
+io.on("connection", socket => {
+    console.log(`Cliente conectado bajo el ID: ${socket.id} `)
 })
